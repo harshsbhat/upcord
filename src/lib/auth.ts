ff6a0a77c372e1b482f9f415e0576e3464a4649b
harshsbhat <harsh121102@gmail.com>
2025-02-19 09:38:35 +0530

    feat: add schema for auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,6 @@ import { db } from "@/server/db"; // your drizzle instance
  
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
+        provider: "pg",
     })
 });
